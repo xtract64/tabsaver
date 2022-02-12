c22a3e373f2cc3c4d65686f9b99a2b5417a87f33
Xtract <xtractgamer@gmail.com>
2022-02-11 23:00:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tabsaver
+ Allows you to easily save all of your tabs for later use, reducing clutter
---
+++ popup/script.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', async function() {
+    var manifest = await browser.runtime.getManifest()
+    document.getElementById('version').innerText = 'v'+ manifest.version
+    document.getElementById('title').innerText = manifest.name
+
+    var storage = await browser.storage.sync.get()
+    var groups = storage.tabGroups
+    if (!groups) browser.storage.sync.set({ 'tabGroups': [] })
+
+    storage = await browser.storage.sync.get()
+    groups = storage.tabGroups
+
+    async function loadGroups() {
+        storage = await browser.storage.sync.get()
+        groups = storage.tabGroups
+    
+        //document.getElementById('tutorial').remove()
+        var tabDiv = document.getElementById('tabs')
+        tabDiv.innerHTML = ''
+        
+        if (groups.length < 1) {
+            document.getElementById('tutorial').hidden = false
+            return;
+        } else
+            document.getElementById('tutorial').hidden = true
+
+        for (let i = 0; i < groups.length; i++) {
+            let d = document.createElement('div')
+            d.innerHTML = `
+                <p>${groups[i].name}</p>
+                <button id="group-${i}">Load</button>
+            `
+            d.setAttribute('class', 'tabGroup')
+            
+            tabDiv.append(d)
+
+            document.getElementById('group-'+ i).addEventListener('click', async () => {
+                storage = await browser.storage.sync.get();
+                groups = storage.tabGroups;
+
+                var urls = groups[i].urls;
+
+                browser.windows.create({ url: urls })
+            })
+        }
+    }
+
+    loadGroups()
+
+    document.getElementById('sync-tabs').addEventListener('click', async () => {
+        let tabInfo = await browser.windows.getAll({
+            populate: true,
+            windowTypes: ["normal"]
+        });
+        let tabs = tabInfo[0].tabs;
+
+        let urls = [];
+        tabs.forEach((value, index) => {
+            if (value.url.startsWith('https://') || value.url.startsWith('http://')) urls.push(value.url);
+        });
+
+        storage = await browser.storage.sync.get();
+        groups = storage.tabGroups;
+
+        let name = document.getElementById('group-name').value;
+        groups[groups.length] = { name, urls };
+        browser.storage.sync.set({ 'tabGroups': groups });
+
+        loadGroups();
+    });
+
+    document.getElementById('clear-tabs').addEventListener('click', async () => {
+        browser.storage.sync.set({ 'tabGroups': [] });
+
+        loadGroups();
+    });
+
+    document.getElementById('settings').addEventListener('click', () => {
+        window.open('/settings.html')
+    });
+});
